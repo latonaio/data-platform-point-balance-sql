5055be2c276fc3bc404fad6afda290f604db954a
Toyoaki Doi <kokorononakaniitumo@yahoo.co.jp>
2024-04-11 10:33:36 +0900

    no message

--- data-platform-point-balance-sql-point-balance-data.sql
+++ data-platform-point-balance-sql-point-balance-data.sql
@@ -1,16 +1,18 @@
 CREATE TABLE `data_platform_point_balance_point_balance_data`
 (
   `BusinessPartner`                  int(12) NOT NULL,
-  `PointCode`                        varchar(5) NOT NULL,
+  `PointSymbol`                      varchar(5) NOT NULL,
   `CurrentBalance`                   float(13) NOT NULL,
   `LimitBalance`                     float(13) DEFAULT NULL,
   `CreationDate`                     date NOT NULL,
+  `CreationTime`                     time NOT NULL,
   `LastChangeDate`                   date NOT NULL,
+  `LastChangeTime`                   time NOT NULL,
 
-    PRIMARY KEY (`BusinessPartner`, `PointCode`),
+    PRIMARY KEY (`BusinessPartner`, `PointSymbol`),
 
     CONSTRAINT `DPFMPointBalancePointBalanceDataBusinessPartner_fk` FOREIGN KEY (`BusinessPartner`) REFERENCES `data_platform_business_partner_general_data` (`BusinessPartner`),
-    CONSTRAINT `DPFMPointBalancePointBalanceDataPointCode_fk` FOREIGN KEY (`PointCode`) REFERENCES `data_platform_point_code_point_code_data` (`PointCode`)
+    CONSTRAINT `DPFMPointBalancePointBalanceDataPointSymbol_fk` FOREIGN KEY (`PointSymbol`) REFERENCES `data_platform_point_symbol_point_symbol_data` (`PointSymbol`)
 
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
